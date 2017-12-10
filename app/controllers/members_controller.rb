4073d807182a413bd20f82d04afb4b10370e49b3
kurosu <ne280107@senshu-u.jp>
2017-12-10 14:09:17 +0900

    必要なcontrollerとmodelの作成

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,22 @@
+class MembersController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def confirm
+  end
+end
